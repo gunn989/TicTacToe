fb493401f4d976b5e715894559bf45cdda415a34
gunn989 <wgdereu@gmail.com>
2019-11-03 11:28:54 -0600

    Add files via upload

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -14,34 +14,28 @@ public class TicTacToe {
      public static void main(String[] args){
          Scanner input = new Scanner(System.in); // Import new scanner
          
+         int currentPlayer;
+         
+         
          // Character array gameboard
+         
+     }
+     public static int game(){
+         int input1;
+         int input2;
+         
          char[][] gameBoard = {
          	{'1' ,  '2' , '3'},
          	{'4', '5' , '6'},
          	{'7', '8', '9'},
    	};
-         printBoard(gameBoard);
+        printBoard(gameBoard);
+        System.out.println("Who wants x");
+        
+         
          
      }
      // Method to print out gameboard
-     public static void printBoard(char[][] gameboard){
-         System.out.print(gameboard[0][0]);
-                  System.out.print("-");
-                  System.out.print(gameboard[0][1]);
-                   System.out.print("-");
-                  System.out.println(gameboard[0][2]);
-                  System.out.print(gameboard[1][0]);
-                  System.out.print("-");
-                  System.out.print(gameboard[1][1]);
-                  System.out.print("-");
-                  System.out.println(gameboard[1][2]);
-                  System.out.print(gameboard[2][0]);
-                  System.out.print("-");
-                  System.out.print(gameboard[2][1]);
-                   System.out.print("-");
-                  System.out.println(gameboard[2][2]);
-     }
+    
+     
 }
-
-// Checking Methods
-
